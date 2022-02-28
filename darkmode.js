e455d14ff81ad8b4f149e8a0e5136d828b83293a
cfuen <SapoPerroGitHub>
2022-02-27 19:10:19 -0500

    ADD darkmode css and js in all pages

--- darkmode.js
+++ darkmode.js
@@ -3,14 +3,35 @@ let sunBtn = document.getElementById('sunBtn');
 const themeBtnIcon = document.getElementById('themeBtnIcon');
 const body = document.body;
 
-moonBtn.onclick = () => {
-	body.classList.replace('light', 'dark');
+function sunButton() {
 	moonBtn.style.display = 'none';
 	sunBtn.style.display = 'block';
-};
-
-sunBtn.onclick = () => {
-	body.classList.replace('dark', 'light');
+}
+function moonButton() {
 	sunBtn.style.display = 'none';
 	moonBtn.style.display = 'block';
+}
+
+// funciones/closures
+const makeDark = () => {
+	body.classList.replace('light', 'dark');
+	localStorage.setItem('theme', 'dark');
+	sunButton();
+	console.log('darkified');
 };
+const makeLight = () => {
+	body.classList.replace('dark', 'light');
+	localStorage.setItem('theme', 'light');
+	moonButton();
+	console.log('lightified');
+};
+
+// onclick events (Sí, son dos botones que se ocultan y muestran para parecer uno)
+moonBtn.onclick = makeDark;
+sunBtn.onclick = makeLight;
+
+// condiciones para que se mantenga la oscuridad mediante cache localstorage
+window.onload = () => {
+	body.classList.replace('light | dark', localStorage.getItem('theme'));
+	localStorage.getItem('theme') === 'dark' ? sunButton() : moonButton();
+}
