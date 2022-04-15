8b26ec7bca7c8c753a6300c4f3d6f0c9f8d81e9a
Juan Castillo <cfuendesign@tutanota.com>
2022-04-15 12:44:42 -0500

    📥 FIX: darkmode works on first visit!

--- darkmode.js
+++ darkmode.js
@@ -1,37 +1,39 @@
-let moonBtn = document.getElementById('moonBtn');
-let sunBtn = document.getElementById('sunBtn');
-const themeBtnIcon = document.getElementById('themeBtnIcon');
+// variables
+let moonBtn = document.getElementById("moonBtn");
+let sunBtn = document.getElementById("sunBtn");
+const themeBtnIcon = document.getElementById("themeBtnIcon");
 const body = document.body;
 
-// funciones/closures
+// funciones
 const sunButton = () => {
-	moonBtn.style.display = 'none';
-	sunBtn.style.display = 'block';
-}
+  moonBtn.style.display = "none";
+  sunBtn.style.display = "block";
+};
 const moonButton = () => {
-	sunBtn.style.display = 'none';
-	moonBtn.style.display = 'block';
-}
+  sunBtn.style.display = "none";
+  moonBtn.style.display = "block";
+};
 const makeDark = () => {
-	body.classList.replace('light', 'dark');
-	localStorage.setItem('theme', 'dark');
-	sunButton();
-	console.log('darkified');
+  body.classList.replace("light", "dark");
+  localStorage.setItem("theme", "dark");
+  sunButton();
+  console.log("darkified");
 };
 const makeLight = () => {
-	body.classList.replace('dark', 'light');
-	localStorage.setItem('theme', 'light');
-	moonButton();
-	console.log('lightified');
+  body.classList.replace("dark", "light");
+  localStorage.setItem("theme", "light");
+  moonButton();
+  console.log("lightified");
 };
 
-// onclick events (Sí, son dos botones que se ocultan y muestran para parecer uno)
+// onclick events del botón (Sí, son dos botones que se ocultan y muestran para parecer uno)
 moonBtn.onclick = makeDark;
 sunBtn.onclick = makeLight;
 
 // condiciones para que se mantenga la oscuridad mediante cache localstorage
 window.onload = () => {
-	body.classList.replace('light', localStorage.getItem('theme'));
-	body.classList.replace('dark', localStorage.getItem('theme'));
-	localStorage.getItem('theme') === 'dark' ? sunButton() : moonButton();
-}
+  localStorage.getItem("theme") || localStorage.setItem("theme", "light")
+  body.classList.replace("light", localStorage.getItem("theme"));
+  body.classList.replace("dark", localStorage.getItem("theme"));
+  localStorage.getItem("theme") === "dark" ? sunButton() : moonButton();
+};
